2c560f4a2914d53eb4b0c05f592add0a5f9856c1
Efe Kaya <115823106+rockswe@users.noreply.github.com>
2023-12-18 22:11:13 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Sqrt calculates the square root of x.
+func Sqrt(x float64) float64 {
+	if x < 0 {
+		return -1 // Return -1 for negative numbers
+	}
+	if x == 0 {
+		return 0
+	}
+
+	z := x / 2 // Initial guess
+	for i := 0; i < 1000; i++ {
+		prevZ := z
+		z -= (z*z - x) / (2 * z)
+		if z == prevZ {
+			break // Break if the value stops changing
+		}
+	}
+	return z
+}
+
+func main() {
+	number := 64.0
+	fmt.Printf("The square root of %v is %v\n", number, Sqrt(number))
+}
